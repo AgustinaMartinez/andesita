4818ef95b0be3b4d6e9f5fa0de294db566c6f7a6
agustinamartinez <agus10.cba@gmail.com>
2025-06-13 13:04:30 -0300

    add carousel, add gradient animation, move components into sections folder, styles improvements

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,26 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
       keyframes: {
         fadeIn: {
-          '0%': { opacity: 0 },
-          '100%': { opacity: 1 },
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
         },
         slideDown: {
-          '0%': { transform: 'translateY(-100%)' },
-          '100%': { transform: 'translateY(0)' },
+          "0%": { transform: "translateY(-100%)" },
+          "100%": { transform: "translateY(0)" },
         },
       },
       animation: {
-        fadeIn: 'fadeIn 1s ease-in-out',
-        slideDown: 'slideDown 0.5s ease-out forwards',
+        fadeIn: "fadeIn 1s ease-in-out",
+        slideDown: "slideDown 0.5s ease-out forwards",
       },
       boxShadow: {
-        'shadow-inset-custom': 'inset 0px 0px 150px 0px rgba(0,0,0,0.51)', // Define clases personalizadas aquí
+        "shadow-inset-custom": "inset 0px 0px 150px 0px rgba(0,0,0,0.51)",
       },
     },
-	},
-	plugins: [],
-}
+  },
+  plugins: [],
+};
